b7aab9d98724a3ce62d4b9a01a61dcd5a1ce3257
marius ballot <ballot.marius@gmail.com>
2019-08-15 21:14:48 +0200

    adding lights to scene

--- src/components/threeScene.js
+++ src/components/threeScene.js
@@ -1,71 +1,76 @@
-import * as THREE from 'three';
-import OrbitControls from 'orbit-controls-es6';
-import vertSource from '../shaders/cube.vert'
-import fragSource from '../shaders/cube.frag'
+import * as THREE from "three";
+import OrbitControls from "orbit-controls-es6";
+import vertSource from "../shaders/cube.vert";
+import fragSource from "../shaders/cube.frag";
 
 class ThreeScene {
+  constructor() {
+    this.camera;
+    this.scene;
+    this.renderer;
+    this.cube;
+    this.controls;
+    this.uniforms;
+    this.bind();
+    this.init();
+  }
 
-    constructor() {
-        this.camera
-        this.scene
-        this.renderer
-        this.cube
-        this.controls
-        this.uniforms
-        this.bind()
-        this.init();
-    }
+  init() {
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
 
-    init() {
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this.renderer.domElement);
+    this.scene = new THREE.Scene();
 
-        this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 0, 5);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enabled = true;
+    this.controls.maxDistance = 1500;
+    this.controls.minDistance = 0;
 
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.set(0, 0, 5)
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.controls.enabled = true;
-        this.controls.maxDistance = 1500;
-        this.controls.minDistance = 0;
+    this.uniforms = {
+      colorB: {
+        type: "vec3",
+        value: new THREE.Color(0xacb6e5)
+      },
+      colorA: {
+        type: "vec3",
+        value: new THREE.Color(0x74ebd5)
+      }
+    };
 
-        this.uniforms = {
-            colorB: { type: 'vec3', value: new THREE.Color(0xACB6E5) },
-            colorA: { type: 'vec3', value: new THREE.Color(0x74ebd5) }
-        }
+    this.cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.ShaderMaterial({uniforms: this.uniforms, vertexShader: vertSource, fragmentShader: fragSource}));
+    this.scene.add(this.cube);
 
-        this.cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.ShaderMaterial({
-            uniforms: this.uniforms,
-            vertexShader: vertSource,
-            fragmentShader: fragSource
+    let light = new THREE.AmbientLight();
+    let pointLight = new THREE.PointLight();
+    pointLight.position.set(10, 10);
+    this.scene.add(light, pointLight);
+  }
 
-        }))
-        this.scene.add(this.cube)
+  update() {
+    this.renderer.render(this.scene, this.camera);
+    this.rotateCube();
+  }
 
+  rotateCube() {
+    this.cube.rotateY(0.01);
+  }
 
-    }
+  resizeCanvas() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+  }
 
-    update() {
-        this.renderer.render(this.scene, this.camera)
-        this.rotateCube()
-    }
-
-    rotateCube() {
-        this.cube.rotateY(0.01)
-    }
-
-    resizeCanvas() {
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.camera.aspect = window.innerWidth / window.innerHeight
-        this.camera.updateProjectionMatrix();
-    }
-
-    bind() {
-        this.resizeCanvas = this.resizeCanvas.bind(this)
-        window.addEventListener('resize', this.resizeCanvas)
-
-    }
+  bind() {
+    this.resizeCanvas = this.resizeCanvas.bind(this);
+    window.addEventListener("resize", this.resizeCanvas);
+  }
 }
 
-export { ThreeScene as default }
+export {
+  ThreeScene as
+  default
+};
